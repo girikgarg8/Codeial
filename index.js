7ebe0989ce85f8df5e5c46dffa754aedd42db522
Girik Garg <girikgarg8@gmail.com>
2022-06-24 10:34:10 +0530

    heroku change

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ app.use(function (req, res, next) {
 });
 
 // setup the chat server to be used with socket.io
-const chatServer = require('http').Server(app);
-const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-chatServer.listen(5000);
-console.log('chat server is listening on port 5000');
+// const chatServer = require('http').Server(app);
+// const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+// chatServer.listen(5000);
+// console.log('chat server is listening on port 5000');
 const path = require('path');
 if (process.env.CODEIAL_ENVIRONMENT == 'production') {
     app.use(sassMiddleware({
