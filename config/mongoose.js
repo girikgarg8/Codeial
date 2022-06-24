956752cc8cb695f7d4db5b6e9933d9e02b4b9ce1
Girik Garg <girikgarg8@gmail.com>
2022-06-24 11:06:30 +0530

    heroku change

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const env=require('./environment');
-mongoose.connect(`mongodb://mongodb+srv://girikgarg:Girikme@#$1234@cluster0.kno8d1a.mongodb.net/?retryWrites=true&w=majority`);
+mongoose.connect('mongodb+srv://girikgarg:girik1234@cluster1.ztljyxx.mongodb.net/?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 
