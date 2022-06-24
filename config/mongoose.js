57a868d9a164a04c275a3ea64d5b97fc0ed370e0
Girik Garg <girikgarg8@gmail.com>
2022-06-24 11:24:07 +0530

    Heroku change

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
-const env=require('./environment');
-mongoose.connect('mongodb+srv://girikgarg:girik1234@cluster1.ztljyxx.mongodb.net/?retryWrites=true&w=majority');
+const mongoose = require("mongoose"); // requiring mongoose
+mongoose
+// .connect("mongodb://localhost/codeial_development")
+    .connect(
+        "mongodb+srv://girikgarg:girik1234@cluster1.ztljyxx.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => {
+        console.log("Connected to Database");
+    })
+    .catch((err) => {
+        console.log("Not Connected to Database ERROR! ", err);
+    });
 
-const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+const db = mongoose.connection; // if connection succesfull
 
-
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
-});
-
-
-module.exports = db;
+module.exports = db; // exporting module
